b03e4d0f6b672d13f05f27d8bfeb6112ede8214a
Yannick Gloster <yannickgloster@gmail.com>
2020-04-27 19:57:14 +0100

    Material UI
    Changed to use material UI

--- pages/_app.js
+++
@@ -1,7 +0,0 @@
-import "font-awesome/css/font-awesome.min.css";
-
-// Global CSS File
-// This default export is required in a new `pages/_app.js` file.
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
--- pages/sailing.js
+++
@@ -1,5 +0,0 @@
-import Layout from "../components/layout";
-
-export default function Sailing() {
-  return <Layout>Sailing</Layout>;
-}
